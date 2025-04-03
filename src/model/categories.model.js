366c610a5676d56a30573230446ec8e624796532
Srilekha Mondal <srilekhamondal.sm@gmail.com>
2025-04-03 12:58:34 +0530

    Add categories and reviews files

---
+++ src/model/categories.model.js
@@ -0,0 +1,23 @@
+import { Schema, model } from "mongoose";
+
+const categorySchema = new Schema({
+    name: {
+        type: String,
+        Unique:true,
+        maxLength: [60, 'name should be in 60 latter'],
+        minLength : [ 3 , "name must be 3 latter or more"] ,
+        required: [true, 'name is reqired!'],
+        trim: true  
+    },
+    description: {
+        type: String,
+        trim: true,
+        maxLength: [500, "title must be under 500 char"]
+    },
+    user: {
+        type: Schema.Types.ObjectId ,
+        ref: 'user',
+        required: true
+    }
+})
+export const Catagories = model("category",categorySchema)
