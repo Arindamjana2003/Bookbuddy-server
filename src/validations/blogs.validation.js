18c94e0f9e28b7a44bed7aa1e5496543458a74bb
Sombhu2022 <sombhudas93@gmail.com>
2025-04-22 22:00:01 +0530

    blog

---
+++ src/validations/blogs.validation.js
@@ -0,0 +1,34 @@
+import { z } from "zod"
+
+class BlogValidations {
+
+    create = z.object({
+        body: z.object({
+          title: z.string().trim().min(3 , "Title is required"),
+          description: z.string().trim().optional(),
+          image : z.string().trim().optional()
+        }).strict(),
+      })
+
+    update = z.object({
+        body: z.object({
+          title: z.string().trim().min(3 , "Title is required"),
+          description: z.string().trim().optional(),
+          image : z.string().trim().optional()
+        }).strict(),
+        params : z.object({
+            blogId : z.string().trim()
+        })
+      })
+
+    delete = z.object({
+        params : z.object({
+            blogId : z.string().trim()
+        })
+      })
+
+      
+
+}
+
+export default new BlogValidations()
