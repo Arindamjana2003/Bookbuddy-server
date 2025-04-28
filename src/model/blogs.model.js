c504d07177da2c918bfda331d0652b2ceb80c3d3
Pritam <123.paulpritam@gmail.com>
2025-04-28 13:34:25 +0530

    add populate noteservice

--- src/model/blogs.model.js
+++ src/model/blogs.model.js
@@ -1,37 +1,37 @@
 import { Schema, model } from "mongoose";
 
 const blogSchema = new Schema(
-  {
-    user: {
-      type: Schema.Types.ObjectId,
-      ref: 'user',
-      required: true,
+    {
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: "user",
+            required: true,
+        },
+        title: {
+            type: String,
+            required: [true, "Title must be required!"],
+            trim: true,
+            minLength: [3, "Title must be 3 characters or more"],
+            maxLength: [60, "Title must be under 60 characters"],
+        },
+        description: {
+            type: String,
+            trim: true,
+            maxLength: [500, "Description must be under 500 characters"],
+        },
+        image: {
+            url: {
+                type: String,
+                // default: 'https://res.cloudinary.com/dab0ekhmy/image/upload/v1728130610/thik-ai/gvjpvq3xljmnw2vwdkag.avif',
+                default: null,
+            },
+            public_id: {
+                type: String,
+                default: null,
+            },
+        },
     },
-    title: {
-      type: String,
-      required: [true, "Title must be required!"],
-      trim: true,
-      minLength: [3, "Title must be 3 characters or more"],
-      maxLength: [60, "Title must be under 60 characters"],
-    },
-    description: { 
-      type: String,
-      trim: true,
-      maxLength: [500, "Description must be under 500 characters"],
-    },
-    image: {
-      url: {
-        type: String,
-        // default: 'https://res.cloudinary.com/dab0ekhmy/image/upload/v1728130610/thik-ai/gvjpvq3xljmnw2vwdkag.avif',
-        default : null
-      },
-      public_id: {
-        type: String,
-        default: null,
-      },
-    },
-  },
-  { timestamps: true } 
+    { timestamps: true }
 );
 
 export const Blogs = model("blog", blogSchema);
